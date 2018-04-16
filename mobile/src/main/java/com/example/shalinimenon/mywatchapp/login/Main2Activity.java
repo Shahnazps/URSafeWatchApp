8aa8b444d263d5ac750f7de2714ec6cc77b388c1
Prasanth Menon <prmeno@microsoft.com>
2018-04-16 18:06:47 +0530

    Countdown timer for call Dummy heartrate on wear

--- mobile/src/main/java/com/example/shalinimenon/mywatchapp/login/Main2Activity.java
+++ mobile/src/main/java/com/example/shalinimenon/mywatchapp/login/Main2Activity.java
@@ -5,14 +5,15 @@ import android.content.Intent;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.CountDownTimer;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.view.View;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -21,72 +22,111 @@ import com.example.shalinimenon.mywatchapp.R;
 
 public class Main2Activity extends AppCompatActivity implements View.OnClickListener {
 
-    private  static final int REQUEST_PHONE_CALL =101;
-    String PHONE_NUMBER = getIntent().getExtras().getString("selectedFromList");
-    String sms=getIntent().getExtras().getString("message");
+    private static final int REQUEST_PHONE_CALL = 101;
+    private String mPhoneNumber;
+    private String mSms;
+    private Intent mIntent;
+    private int countDown;
 
     Button hrok;
-    EditText hr,count;
+    EditText hr, count;
 
 
-            @Override
-            protected void onCreate(Bundle savedInstanceState) {
-                super.onCreate(savedInstanceState);
-                setContentView(R.layout.activity_main2);
-                Toast.makeText(this, "Main2Activity", Toast.LENGTH_LONG).show();
-                hrok = (Button) findViewById(R.id.hrok);
-                hr = (EditText) findViewById(R.id.hr);
-                count= (EditText) findViewById(R.id.count);
-                hrok.setOnClickListener(this);
-            }
-
-            @Override
-            public void onClick(View v) {
-                String value = hr.getText().toString();
-                final int rate = Integer.parseInt(value);
-                String value2=count.getText().toString();
-                final int pedo=Integer.parseInt(value2);
-
-                System.out.println("heart rate = " + rate);
-                if (rate > 90 && pedo>30 || rate>90 && pedo==0) {
-                   makePhoneCall(Main2Activity.this, PHONE_NUMBER);
-                    if (ContextCompat.checkSelfPermission(Main2Activity.this,
-                            Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
-                        ActivityCompat.requestPermissions(Main2Activity.this,
-                                new String[]{Manifest.permission.SEND_SMS}, 1);
-                    } else {//do nothing
-                        try {
-                            SmsManager smsmanager = SmsManager.getDefault();
-                            smsmanager.sendTextMessage(PHONE_NUMBER, null, sms, null, null);
-                            Toast.makeText(Main2Activity.this, "sent", Toast.LENGTH_SHORT).show();
-                        } catch (Exception e) {
-                            Toast.makeText(Main2Activity.this, "failed", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-
-                }
-            }
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        //mPhoneNumber = getIntent().getExtras().getString("selectedFromList");
+        //mSms = getIntent().getExtras().getString("message");
+        mPhoneNumber = "8137871193";
+        mSms = "Need Help!";
+
+        Toast.makeText(this, "Main2Activity", Toast.LENGTH_LONG).show();
+        hrok = (Button) findViewById(R.id.hrok);
+        hr = (EditText) findViewById(R.id.hr);
+        count = (EditText) findViewById(R.id.count);
+        hrok.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String value = hr.getText().toString();
+        final int rate = Integer.parseInt(value);
+        String value2 = count.getText().toString();
+        final int pedo = Integer.parseInt(value2);
+
+        System.out.println("heart rate = " + rate);
+        if (rate > 90 && pedo > 30 || rate > 90 && pedo == 0) {
+            makePhoneCallAndSms(Main2Activity.this, mPhoneNumber);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+            @NonNull String[] permissions,
+            @NonNull int[] grantResults) {
         switch (requestCode) {
             case REQUEST_PHONE_CALL:
-                makePhoneCall(Main2Activity.this, PHONE_NUMBER);
+                startActivity(mIntent);
                 break;
         }
     }
 
-        private void makePhoneCall(Context context, String phoneNumber) {
-            Intent intent = new Intent(Intent.ACTION_CALL);
-            intent.setData(Uri.parse("tel:" + PHONE_NUMBER));
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(Main2Activity.this, new String[]{Manifest.permission.CALL_PHONE}, REQUEST_PHONE_CALL);
-                return;
+    private void makePhoneCallAndSms(final Context context, String phoneNumber) {
+
+        countDown = 10;
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        final AlertDialog alertDialog = dialogBuilder.setTitle("Waiting for response...")
+                .setMessage(getDialogMessage(countDown))
+                .show();
+
+        new CountDownTimer(10000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                alertDialog.setMessage(getDialogMessage(--countDown));
             }
 
-            context.startActivity(intent);
-        }
+            @Override
+            public void onFinish() {
+                alertDialog.dismiss();
+                mIntent = new Intent(Intent.ACTION_CALL);
+                mIntent.setData(Uri.parse("tel:" + mPhoneNumber));
+                if (ActivityCompat.checkSelfPermission(context,
+                        Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(Main2Activity.this, new String[] {Manifest.permission.CALL_PHONE},
+                            REQUEST_PHONE_CALL);
+                    return;
+                }
 
-        }
+                startActivity(mIntent);
+
+                // Send sms
+                if (ContextCompat.checkSelfPermission(Main2Activity.this,
+                        Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(Main2Activity.this,
+                            new String[] {Manifest.permission.SEND_SMS}, 1);
+                } else {//do nothing
+                    try {
+                        SmsManager smsmanager = SmsManager.getDefault();
+                        smsmanager.sendTextMessage(mPhoneNumber, null, mSms, null, null);
+                        Toast.makeText(Main2Activity.this, "Help SMS sent", Toast.LENGTH_SHORT).show();
+                    } catch (Exception e) {
+                        Toast.makeText(Main2Activity.this, "Help SMS failed", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            }
+        }.start();
+
+
+    }
+
+    private String getDialogMessage(int countDown) {
+        return "Placing call in " + countDown + " seconds";
+    }
+
+}
 
 
 
--- wear/src/main/java/com/example/shalinimenon/mywatchapp/MainActivity.java
+++ wear/src/main/java/com/example/shalinimenon/mywatchapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.shalinimenon.mywatchapp;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -10,31 +12,39 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.wearable.activity.WearableActivity;
 import android.support.wearable.view.WatchViewStub;
+import android.support.wearable.view.WearableDialogHelper;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 public class MainActivity extends WearableActivity {
 
     private TextView mTextView;
     private HeartRateBroadcastReceiver heartRateBroadcastReceiver;
-   // private static final String TAG = "MainActivity";
-   private class HeartRateBroadcastReceiver extends BroadcastReceiver {
-       @Override
-       public void onReceive(Context arg0, Intent arg1) {
-           // ahr
-           Log.v(this.getClass().getName(), "Value Recieved");
-           if (arg1.getAction().equals("heartRateAction")) {
-               float hr = arg1.getFloatExtra("HeartRate", 0);
-               if (MainActivity.this.mTextView != null) {
-                   MainActivity.this.mTextView.setText("Heart Rate : " + hr);
-               }
-           }
-       }
-   }
+    private AlertDialog mDialog;
+
+    // private static final String TAG = "MainActivity";
+    private class HeartRateBroadcastReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context arg0, Intent arg1) {
+            // ahr
+            Log.v(this.getClass().getName(), "Value Recieved");
+            if (arg1.getAction().equals("heartRateAction")) {
+                float hr = arg1.getFloatExtra("HeartRate", 0);
+                if (MainActivity.this.mTextView != null) {
+                    MainActivity.this.mTextView.setText("Heart Rate : " + hr);
+                }
+            }
+        }
+    }
+
     @Override
     protected void onStart() {
         // initialize the broadcast recieiver
@@ -54,6 +64,7 @@ public class MainActivity extends WearableActivity {
         stopService(new Intent(this, HeartRateWearService.class));
         super.onStop();
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -63,6 +74,21 @@ public class MainActivity extends WearableActivity {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
                 mTextView = (TextView) stub.findViewById(R.id.heartrate);
+
+                AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(MainActivity.this);
+                dialogBuilder.setTitle("Please wait...")
+                        .setMessage("Fetching the Heart Rate");
+
+                mDialog = dialogBuilder.show();
+
+                final Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mTextView.setText("Current Heart Rate : " + ThreadLocalRandom.current().nextInt(60, 100 + 1));
+                        mDialog.dismiss();
+                    }
+                }, 3000);
             }
         });
 
